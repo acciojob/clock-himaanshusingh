b5d3d925e5b4902e2bd5da8bf26f5363dde172e9
Himanshu Singh <himanshusingh1k@gmail.com>
2025-10-31 11:46:32 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
 //your JS code here. If required.
+function updateTimer() {
+  const dateString = new Date().toLocaleString();
+  const timerElement = document.getElementById("timer");
+  timerElement.textContent = dateString;
+}
+updateTimer();
+setInterval(updateTimer, 1000);
